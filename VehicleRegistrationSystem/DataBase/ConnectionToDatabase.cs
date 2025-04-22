8e53bdfef7a5a7068cb283a594ab754a91d20591
o.pereyra <onasis.pereyra@gmail.com>
2025-04-22 04:09:47 -0400

    Update
    Making a folder called DataBase to insert the previous ConnectionToDatabse class from Entities and deleting it from Entities.

--- VehicleRegistrationSystem/DataBase/ConnectionToDatabase.cs
+++ VehicleRegistrationSystem/DataBase/ConnectionToDatabase.cs
@@ -4,24 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities
+namespace DataBase
 {
     public class Database
     {
         public string ConnectionToDatabase;
-        
-        
+          
         public Database() {
             this.ConnectionToDatabase = "Server=ONASIS-LAPTOP-H\\SQLEXPRESS;Database=vehicle_registration_system;Trusted_Connection=True;"; ;
         }
-
-       
-           
-       
-
     }
-
-
 }
     
 
